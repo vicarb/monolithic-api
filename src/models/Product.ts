b227dd7445db28a1b14dae8c1372bb38f414e054
vicarb <vcardenasb@protonmail.com>
2023-08-15 14:01:22 -0400

    updated state

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,26 +1,34 @@
 import mongoose, { Document, Schema } from 'mongoose';
 
 interface IProduct extends Document {
-  name: string;
-  price: number;
-  supplier: Schema.Types.ObjectId; // Reference to the Supplier
+    name: string;
+    description: string;
+    price: number;
+    supplier: Schema.Types.ObjectId; // Reference to the supplier
+    // Add other fields as needed
 }
 
 const ProductSchema: Schema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  supplier: {
-    type: Schema.Types.ObjectId,
-    ref: 'Supplier', // Reference to the Supplier model
-    required: true,
-  },
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    supplier: {
+        type: Schema.Types.ObjectId,
+        ref: 'Supplier',
+        required: true
+    }
+    // Add other fields as needed
 });
 
 const Product = mongoose.model<IProduct>('Product', ProductSchema);
 export default Product;
+
