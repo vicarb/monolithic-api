01547a6e99f8a8341ac33cd56f06199959cd399b
vicarb <vcardenasb@protonmail.com>
2023-08-16 00:37:46 -0400

    updated swagger conf

--- swaggerConfig.ts
+++ swaggerConfig.ts
@@ -8,6 +8,12 @@ export const options = {
       title: 'My API',
       version: '1.0.0',
     },
+    servers: [
+      {
+        url: 'http://127.0.0.1:3000/api/suppliers',
+        description: 'Local server',
+      },
+    ],
   },
   apis: ['./src/routes/*.ts'], // Path to the API docs
 };
